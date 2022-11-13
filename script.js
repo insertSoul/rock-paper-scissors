67a3670fc44d6578623ad1a04d23ddc2d17fe00c
insertSoul <j.clarke213@gmail.com>
2022-11-13 14:10:27 +0000

    Make function for computers guess

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-
-
 // create a message explaining rules 
-// Done in HTMl
+//(going to be all done in console)
 
 // ######## Function for 1 turn ##########
 
@@ -9,7 +7,10 @@
 
 //      Remove case sensitivity problems
 // Store user inputs in variable
+
 // Generate the computers move and store as a variable (random number 1=R, 2=P 3=S ????)
+// ^^ make it a seperate function
+
 // Compare user and Computers moves to determine winner (using if else statments)
 //      if computer == user => draw (need to display draw and recall function)
 //      if user(Rock) computer(Paper) => lose
@@ -24,10 +25,23 @@
 // Make sure function does exit after
 //let guessInput = document.getElementById(guessInput);
 
-
-let guessInput = document.getElementById(guessInput);
-function gameTurn(guessInput) {
-    console.log(typeof guessInput);
-    guessInput = guessInput.toLowerCase();
-    console.log(guessInput);
+function getComputerChoice() {
+    let randomChoice = (Math.random() * 3)
+    randomChoice = Math.floor(randomChoice);
+    if (randomChoice == 0) {
+        return("Rock")
+    } else if (randomChoice == 1) {
+        return("Scissors")
+    } else {
+        return ("Paper")
+    }
 }
+
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+
